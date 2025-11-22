b7f0cdfd512b04b6e0329fd4c8380daf3bcd620d
Арсен <themidnightdev404@gmail.com>
2025-11-22 17:10:51 +0300

    Render thumbnails

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import { photos } from './data.js';
+import { renderPictures } from './thumbnails.js';
 
-// eslint-disable-next-line no-console
-console.log(photos);
+renderPictures(photos);
---
+++ js/thumbnails.js
@@ -0,0 +1,34 @@
+const picturesContainer = document.querySelector('.pictures');
+
+const pictureTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+function createPictureElement(photo) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  const img = pictureElement.querySelector('.picture__img');
+  const likes = pictureElement.querySelector('.picture__likes');
+  const comments = pictureElement.querySelector('.picture__comments');
+
+  img.src = photo.url;
+  img.alt = photo.description;
+  likes.textContent = photo.likes;
+  comments.textContent = photo.comments.length;
+
+  return pictureElement;
+}
+
+function renderThumbnails(photos) {
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const pictureElement = createPictureElement(photo);
+    fragment.append(pictureElement);
+  });
+
+  picturesContainer.append(fragment);
+}
+
+export { renderThumbnails };
